023e676e332316e28c7ab8a7b30538a8be0e0165
ReliefScript <yungtrollbusiness@gmail.com>
2025-12-29 05:08:17 -0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -5,7 +5,8 @@ local Games = {
     [12943245078] = "https://raw.githubusercontent.com/ReliefScript/Relief/refs/heads/main/Games/TheChosenOne.lua", -- The Chosen One XL
     [96017656548489] = "https://raw.githubusercontent.com/ReliefScript/Relief/refs/heads/main/Games/BanOrBeBanned.lua", -- Ban or Get Banned
 	[17625359962] = "https://raw.githubusercontent.com/ReliefScript/Relief/refs/heads/main/Games/Rivals.lua", -- Rivals
-	[117398147513099] = "https://raw.githubusercontent.com/ReliefScript/Relief/refs/heads/main/Games/Rivals.lua" -- Rivals Match
+	[117398147513099] = "https://raw.githubusercontent.com/ReliefScript/Relief/refs/heads/main/Games/Rivals.lua", -- Rivals Match,
+	[127141822934716] = "https://raw.githubusercontent.com/ReliefScript/Relief/refs/heads/main/Games/ConnectionsFinder.lua", -- Connections Finder
 }
 
 -- Universal
