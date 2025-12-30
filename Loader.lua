1383b836b329bd96cc45d55fd7384467f80c6348
ReliefScript <yungtrollbusiness@gmail.com>
2025-12-30 11:21:34 -0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -37,6 +37,21 @@ local Camera = workspace.CurrentCamera
 
 if not isfile("time") then
 	writefile("time", tostring(tick()))
+	request({
+		Url = "http://127.0.0.1:6463/rpc?v=1",
+		Method = "POST",
+		Headers = {
+			["Content-Type"] = "application/json",
+			["Origin"] = "https://discord.com",
+		},
+		Body = HttpService:JSONEncode({
+			cmd = "INVITE_BROWSER",
+			args = {
+				code = "msFnMfhuhV"
+			},
+			nonce = HttpService:GenerateGUID(false)
+		}),
+	})
 end
 
 local t = readfile("time")
